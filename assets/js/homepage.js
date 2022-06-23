228c01abdc9114cf83b4cfab68333f69cf38c95b
Marina Kaeufer <marishutka@yahoo.com>
2022-06-22 20:17:39 -0400

    worked on day and temperature

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -32,7 +32,7 @@ var buttonClickHandler = function (event) {
 var getCityWeathers = function (city) {
 
   //ajax here
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&exclude=daily&appid=2a312f7d725b85142a0017e3fca4c028`;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=2a312f7d725b85142a0017e3fca4c028`;
 
   fetch(url)
     .then(response => response.json())
@@ -81,24 +81,48 @@ var displayWeathers = function (weathers, searchTerm) {
     weatherContainerEl.textContent = 'No weather data found.';
     return;
   }
-  const date = new Date();
-
+  let date = new Date();
+  var month = date.getUTCMonth() + 1; //months from 1-12
+  var day = date.getUTCDate();
+  var year = date.getUTCFullYear();
+  let currentDay = year + "-" + month + "-" + "day";
   weatherSearchTerm.textContent = searchTerm + " " + "(" + date.toLocaleDateString('en-US') + ")";
 
+  //   Single day forecast
+  
+  //   5 Day forecast
   for (var i = 0; i < weathers.length; i++) {
+    if(currentDay === weathers[i]['dt_txt'].split(' ')[0]) break;
+    currentDay = weathers[i]['dt_txt'].split(' ')[0];
+    // main --> temp, humudity 
+    // wind --> speed
+    const weatherDate = weathers[i]['dt']
+    const temp = weathers[i]['main']['temp'];
+    const wind = weathers[i]['wind']['speed'];
+    const humidity = weathers[i]['main']['humidity'];
+
+    console.log(` `)
+    console.log(`==> currentDay ${currentDay}`)
+    console.log(`==> temp ${temp}`)
+    console.log(`==> wind ${wind}`)
+    console.log(`==> humidity ${humidity}`)
+
     // var weatherName = weathers[i].owner.login + '/' + weathers[i].name;
 
-    var weatherEl = document.createElement('a');
+    var weatherEl = document.createElement('div');
     weatherEl.classList = 'list-item flex-row justify-space-between align-center';
     // weatherEl.setAttribute('href', './single-weather.html?weather=' + weatherName);
 
     var titleEl = document.createElement('span');
-    // titleEl.textContent = weatherName;
+    // date = currentDay;
+    titleEl.textContent = currentDay;
+    // titleEl.textContent = date.toLocaleDateString('en-US');
 
     weatherEl.appendChild(titleEl);
 
     var statusEl = document.createElement('span');
     statusEl.classList = 'flex-row align-center';
+    statusEl.textContent = temp;
 
     // if (weathers[i].open_issues_count > 0) {
     //   statusEl.innerHTML =
