b18b34bdf827ce50e33a39f2cb06c159cfae2b5b
Marina Kaeufer <marishutka@yahoo.com>
2022-06-24 21:55:29 -0400

    worked on today's date forecast

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,9 @@ var userFormEl = document.querySelector('#user-form');
 var historyButtonsEl = document.querySelector('#history-buttons');
 var nameInputEl = document.querySelector('#city');
 var weatherContainerEl = document.querySelector('#weathers-container');
+var todaysWeatherContainerEl = document.querySelector('#todays-weathers-container');
 var weatherSearchTerm = document.querySelector('#weather-search-term');
+
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
@@ -37,29 +39,9 @@ var getCityWeathers = function (city) {
   fetch(url)
     .then(response => response.json())
     .then(data => {
-        // console.log(`==> data.list ${JSON.stringify(data.list,null,2)}`);
-        // console.log(`==> city ${JSON.stringify(city,null,2)}`);
         displayWeathers(data.list, city);
 
     })
-
-//   var apiUrl = `api.openweathermap.org/data/2.5/weather?q=Miami&appid=2a312f7d725b85142a0017e3fca4c028`;
-
-//   fetch(apiUrl)
-//     .then(function (response) {
-//       if (response.ok) {
-//         console.log(response);
-//         response.json().then(function (data) {
-//           console.log(data);
-//         //   displayWeathers(data, city);
-//         });
-//       } else {
-//         alert('Error: ' + response.statusText);
-//       }
-//     })
-//     .catch(function (error) {
-//       alert('Unable to connect to Open Call API');
-//     });
 };
 
 var getFeaturedWeathers = function (history) {
@@ -87,6 +69,43 @@ var displayWeathers = function (weathers, searchTerm) {
   let newDate = today;
   newDate.setHours(0,0,0,0)
 
+  //   Today's forecast
+  let temp = weathers[0]['main']['temp'];
+  let wind = weathers[0]['wind']['speed'];
+  let humidity = weathers[0]['main']['humidity'];
+
+  var weatherEl = document.createElement('div');
+    weatherEl.classList = 'list-item flex-row justify-space-between align-center';
+    // weatherEl.setAttribute('href', './single-weather.html?weather=' + weatherName);
+
+    var titleEl = document.createElement('span');
+    // date = currentDay;
+    titleEl.textContent = newDate.toLocaleDateString('en-US');
+    // titleEl.textContent = date.toLocaleDateString('en-US');
+
+    weatherEl.appendChild(titleEl);
+
+    var tempEl = document.createElement('span');
+    tempEl.classList = 'flex-row align-center';
+    tempEl.textContent = temp;
+
+    var windEl = document.createElement('span');
+    windEl.classList = 'flex-row align-center';
+    windEl.textContent = wind;
+
+    var humidEl = document.createElement('span');
+    humidEl.classList = 'flex-row align-center';
+    humidEl.textContent = humidity;
+
+
+    weatherEl.appendChild(tempEl);
+    weatherEl.appendChild(windEl);
+    weatherEl.appendChild(humidEl);
+
+    todaysWeatherContainerEl.appendChild(weatherEl);
+
+    // =========================== END TODAY ===========================
+
   //   5 Day forecast
   for (var i = 0; i < weathers.length; i++) {
     let currentDate = new Date(weathers[i]['dt'] * 1000);
@@ -95,9 +114,10 @@ var displayWeathers = function (weathers, searchTerm) {
     if(newDate.getDate() == currentDate.getDate()) continue;
     newDate = currentDate;
 
-    const temp = weathers[i]['main']['temp'];
-    const wind = weathers[i]['wind']['speed'];
-    const humidity = weathers[i]['main']['humidity'];
+    temp = weathers[i]['main']['temp'];
+    wind = weathers[i]['wind']['speed'];
+    humidity = weathers[i]['main']['humidity'];
+    
 
     // console.log(` `)
     // console.log(`==> currentDate ${currentDate}`)
@@ -105,7 +125,7 @@ var displayWeathers = function (weathers, searchTerm) {
     // console.log(`==> wind ${wind}`)
     // console.log(`==> humidity ${humidity}`)
 
-    // var weatherName = weathers[i].owner.login + '/' + weathers[i].name;
+    
 
     var weatherEl = document.createElement('div');
     weatherEl.classList = 'list-item flex-row justify-space-between align-center';
